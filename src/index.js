dc20723e233928cfff7b08a15428fbbac8cc8126
Rayan Junio <rayanjunior234@gmail.com>
2024-08-07 14:06:44 -0300

    feat: adicionar CRUD de tutor

--- src/index.js
+++ src/index.js
@@ -14,6 +14,58 @@ app.use(
 
 app.use(express.json());
 
+app.get("/tutors", async (req, res) => {
+  const users = await Tutor.findAll({
+    include: {
+      model: Pet,
+      attributes: ["id","name", "species", "carry", "weight", "date_of_birth"],
+    },
+  });
+
+  res.json(users);
+});
+
+app.post("/tutor", async (req, res) => {
+  const name = req.body.name;
+  const phone = req.body.phone;
+  const email = req.body.email;
+  const date_of_birth = req.body.date_of_birth;
+  const zip_code = req.body.zip_code;
+
+  const tutor = await Tutor.create({
+    name,
+    phone,
+    email,
+    date_of_birth,
+    zip_code,
+  });
+
+  res.json(tutor);
+});
+
+app.put("/tutor/:id", async (req, res) => {
+  const id = req.params.id;
+  const tutor = await Tutor.findByPk(id);
+
+  tutor.name = req.body.name;
+  tutor.phone = req.body.phone;
+  tutor.email = req.body.email;
+  tutor.date_of_birth = req.body.date_of_birth;
+  tutor.zip_code = req.body.zip_code;
+
+  await tutor.save();
+  res.json(tutor);
+});
+
+app.delete("/tutor/:id", async (req, res) => {
+  const id = req.params.id;
+  const tutor = await Tutor.findByPk(id);
+
+  await tutor.destroy();
+
+  res.sendStatus(204);
+});
+
 connection
   .sync()
   .then(() => {
