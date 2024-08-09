9dfe9ff582f012aa6fbc9ea3ccc3fe717e635ec8
Rayan Junio <rayanjunior234@gmail.com>
2024-08-09 14:49:10 -0300

    feat: adicionar funcionalidade de criação automática da base de dados, caso a mesma não exista

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,4 +1,16 @@
 const { Sequelize } = require("sequelize");
+const mysql = require("mysql2/promise");
+
+async function createDatabase() {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+  });
+  await connection.query("CREATE DATABASE IF NOT EXISTS pets_clinic");
+  await connection.end();
+}
+createDatabase();
 
 const sequelize = new Sequelize("pets_clinic", "root", "", {
   host: "localhost",
